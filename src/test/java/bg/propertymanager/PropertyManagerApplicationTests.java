99d87e3e496c32bf154a950539b0ee3369dca20c
dimov05 <ddimov.1999@gmail.com>
2022-12-18 23:18:10 +0200

    Property Manager initialize entities

---
+++ src/test/java/bg/propertymanager/PropertyManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.propertymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
