9b852293cfbae0845422b33f8b01492489a608ae
dimov05 <ddimov.1999@gmail.com>
2022-12-19 23:02:54 +0200

    Adding some templates to edit later

---
+++ src/main/java/bg/propertymanager/web/HomeController.java
@@ -0,0 +1,16 @@
+package bg.propertymanager.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+    @GetMapping("/home")
+    public String home(){
+        return "home";
+    }
+}
