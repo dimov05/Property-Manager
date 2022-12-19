bc7d1df84fe34e1a863318b886ba5007330c5f34
dimov05 <ddimov.1999@gmail.com>
2022-12-19 16:36:49 +0200

    Implementing Spring Security

---
+++ src/main/java/bg/propertymanager/repository/MessageRepository.java
@@ -0,0 +1,9 @@
+package bg.propertymanager.repository;
+
+import bg.propertymanager.model.entity.MessageEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MessageRepository extends JpaRepository<MessageEntity, Long> {
+}
