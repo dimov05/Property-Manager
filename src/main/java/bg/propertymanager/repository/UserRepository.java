ca709f12461347d63d6daddad775e03887538c15
dimov05 <ddimov.1999@gmail.com>
2022-12-20 16:35:12 +0200

    Working on Register form and logic

--- src/main/java/bg/propertymanager/repository/UserRepository.java
+++ src/main/java/bg/propertymanager/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package bg.propertymanager.repository;
 
-import bg.propertymanager.model.entity.ApartmentEntity;
 import bg.propertymanager.model.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
