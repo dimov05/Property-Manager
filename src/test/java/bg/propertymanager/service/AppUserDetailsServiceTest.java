4cf848f7f98d662b206d45e83b9ed264415c6a5a
dimov05 <ddimov.1999@gmail.com>
2022-12-21 13:52:56 +0200

    Implementing thymeleaf with spring security and other functions

---
+++ src/test/java/bg/propertymanager/service/AppUserDetailsServiceTest.java
@@ -0,0 +1,73 @@
+package bg.propertymanager.service;
+
+import bg.propertymanager.model.entity.RoleEntity;
+import bg.propertymanager.model.entity.UserEntity;
+import bg.propertymanager.model.enums.UserRolesEnum;
+import bg.propertymanager.model.user.AppUserDetails;
+import bg.propertymanager.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith({MockitoExtension.class})
+public class AppUserDetailsServiceTest {
+
+    @Mock
+    private UserRepository mockUserRepo;
+    private AppUserDetailsService toTest;
+
+    @BeforeEach
+    void setUp() {
+        toTest = new AppUserDetailsService(mockUserRepo);
+    }
+
+    @Test
+    void testLoadUserByUsername_UserExists() {
+        // Arrange
+        UserEntity testUserEntity = new UserEntity()
+                .setRoles(List.of(new RoleEntity().setRole(UserRolesEnum.ADMIN),
+                        new RoleEntity().setRole(UserRolesEnum.USER)))
+                .setUsername("pesho")
+                .setPassword("123456")
+                .setEmail("pesho@abv.bg")
+                .setCountry("Builgaria")
+                .setFullName("Pesho Petrov")
+                .setCity("Sofia")
+                .setStreet("Ulitsa 55");
+        when(mockUserRepo.findByUsername(testUserEntity.getUsername()))
+                .thenReturn(Optional.of(testUserEntity));
+        // Act
+        AppUserDetails userDetails = toTest.loadUserByUsername(testUserEntity.getUsername());
+        // Assert
+        Assertions.assertEquals(testUserEntity.getUsername(), userDetails.getUsername());
+        Assertions.assertEquals(testUserEntity.getEmail(), userDetails.getEmail());
+        Assertions.assertEquals(testUserEntity.getPassword(), userDetails.getPassword());
+        Assertions.assertEquals(testUserEntity.getCity(), userDetails.getCity());
+        Assertions.assertEquals(testUserEntity.getCountry(), userDetails.getCountry());
+        Assertions.assertEquals(testUserEntity.getStreet(), userDetails.getStreet());
+        Assertions.assertEquals(testUserEntity.getFullName(), userDetails.getFullName());
+
+        var authorities = userDetails.getAuthorities();
+        Assertions.assertEquals(2, authorities.size());
+        var authoritiesIter = authorities.iterator();
+        Assertions.assertEquals("ROLE_" + UserRolesEnum.ADMIN.name(), authoritiesIter.next().getAuthority());
+        Assertions.assertEquals("ROLE_" + UserRolesEnum.USER.name(), authoritiesIter.next().getAuthority());
+
+    }
+
+    @Test
+    void testLoadUserByUsername_UserDoesNotExists() {
+        Assertions.assertThrows(UsernameNotFoundException.class,
+                () -> toTest.loadUserByUsername("non-existing"));
+    }
+}
