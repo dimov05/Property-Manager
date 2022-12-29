fb28d94a23427e16f28cb29b180a83bcaefa8a48
dimov05 <ddimov.1999@gmail.com>
2022-12-29 16:00:05 +0200

    Working on buildings page

--- src/main/java/bg/propertymanager/service/BuildingService.java
+++ src/main/java/bg/propertymanager/service/BuildingService.java
@@ -1,7 +1,35 @@
 package bg.propertymanager.service;
 
+import bg.propertymanager.model.dto.BuildingViewDTO;
+import bg.propertymanager.model.entity.BuildingEntity;
+import bg.propertymanager.repository.BuildingRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class BuildingService {
+    private final BuildingRepository buildingRepository;
+    private final ModelMapper modelMapper;
+
+    public BuildingService(BuildingRepository buildingRepository, ModelMapper modelMapper) {
+        this.buildingRepository = buildingRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    public List<BuildingViewDTO> findAll() {
+        return buildingRepository.findAll()
+                .stream()
+                .map(building ->
+                    modelMapper.map(building, BuildingViewDTO.class))
+                .collect(Collectors.toList());
+    }
+//    public BuildingViewDTO map(BuildingEntity building){
+//        return new BuildingViewDTO()
+//                .s
+//    }
+
 }
