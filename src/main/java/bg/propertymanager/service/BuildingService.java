01d8a29c4ed59060098a2cd9179540039830819f
dimov05 <ddimov.1999@gmail.com>
2023-01-10 12:02:56 +0200

    Working ot Add/Edit/View messages as manager

--- src/main/java/bg/propertymanager/service/BuildingService.java
+++ src/main/java/bg/propertymanager/service/BuildingService.java
@@ -6,6 +6,7 @@ import bg.propertymanager.model.dto.building.BuildingEditDTO;
 import bg.propertymanager.model.dto.building.BuildingViewDTO;
 import bg.propertymanager.model.entity.ApartmentEntity;
 import bg.propertymanager.model.entity.BuildingEntity;
+import bg.propertymanager.model.entity.MessageEntity;
 import bg.propertymanager.model.entity.UserEntity;
 import bg.propertymanager.model.enums.ImagesOfBuildings;
 import bg.propertymanager.repository.BuildingRepository;
@@ -90,8 +91,8 @@ public class BuildingService {
                 .findById(buildingChangeTaxesDTO.getId())
                 .orElseThrow(() -> new NullPointerException("The building you are searching is missing"));
         buildingToSave.setTaxPerDog(buildingChangeTaxesDTO.getTaxPerDog())
-                        .setTaxPerPerson(buildingChangeTaxesDTO.getTaxPerPerson())
-                                .setTaxPerElevatorChip(buildingChangeTaxesDTO.getTaxPerElevatorChip());
+                .setTaxPerPerson(buildingChangeTaxesDTO.getTaxPerPerson())
+                .setTaxPerElevatorChip(buildingChangeTaxesDTO.getTaxPerElevatorChip());
         apartmentService.updatePeriodicTax(buildingToSave);
         buildingRepository.save(buildingToSave);
     }
@@ -127,18 +128,6 @@ public class BuildingService {
         buildingRepository.save(building);
     }
 
-    private String getImageUrlBySize(int buildingFloors) {
-        String imageUrl;
-        if (buildingFloors <= 3) {
-            imageUrl = ImagesOfBuildings.SMALL_BUILDING.url;
-        } else if (buildingFloors <= 8) {
-            imageUrl = ImagesOfBuildings.MEDIUM_BUILDING.url;
-        } else {
-            imageUrl = ImagesOfBuildings.LARGE_BUILDING.url;
-        }
-        return imageUrl;
-    }
-
     private UserEntity getManagerToSet(BuildingEditDTO buildingEditDTO) {
         return userService
                 .findById(buildingEditDTO.getManager().getId());
@@ -151,6 +140,28 @@ public class BuildingService {
 
     public void removeApartment(ApartmentEntity apartmentToRemove, BuildingEntity building) {
         building.getApartments().remove(apartmentToRemove);
+        buildingRepository.save(building);
+    }
+
+    public void addMessage(MessageEntity newMessage, BuildingEntity building) {
+        building.getMessages().add(newMessage);
+        buildingRepository.save(building);
+    }
+
+    private String getImageUrlBySize(int buildingFloors) {
+        String imageUrl;
+        if (buildingFloors <= 3) {
+            imageUrl = ImagesOfBuildings.SMALL_BUILDING.url;
+        } else if (buildingFloors <= 8) {
+            imageUrl = ImagesOfBuildings.MEDIUM_BUILDING.url;
+        } else {
+            imageUrl = ImagesOfBuildings.LARGE_BUILDING.url;
+        }
+        return imageUrl;
+    }
+
+    public void removeMessageFromBuilding(MessageEntity messageToRemove, BuildingEntity building) {
+        building.getMessages().remove(messageToRemove);
         buildingRepository.save(building);
     }
 }
--- src/main/java/bg/propertymanager/service/MessageService.java
+++ src/main/java/bg/propertymanager/service/MessageService.java
@@ -1,20 +1,30 @@
 package bg.propertymanager.service;
 
 import bg.propertymanager.model.dto.building.BuildingViewDTO;
+import bg.propertymanager.model.dto.message.MessageAddDTO;
+import bg.propertymanager.model.dto.message.MessageEditDTO;
+import bg.propertymanager.model.entity.ApartmentEntity;
+import bg.propertymanager.model.entity.BuildingEntity;
 import bg.propertymanager.model.entity.MessageEntity;
+import bg.propertymanager.model.entity.UserEntity;
 import bg.propertymanager.repository.MessageRepository;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.time.LocalDateTime;
 import java.util.Set;
 
 @Service
 @Transactional
 public class MessageService {
     private final MessageRepository messageRepository;
+    private final BuildingService buildingService;
+    private final UserService userService;
 
-    public MessageService(MessageRepository messageRepository) {
+    public MessageService(MessageRepository messageRepository, BuildingService buildingService, UserService userService) {
         this.messageRepository = messageRepository;
+        this.buildingService = buildingService;
+        this.userService = userService;
     }
 
 
@@ -27,5 +37,46 @@ public class MessageService {
     public Set<MessageEntity> findAllManagersMessagesForBuildingSortedFromNewToOld(BuildingViewDTO building) {
         return messageRepository
                 .findAllByBuilding_IdAndAuthorIdOrderByCreatedDateDesc(building.getId(), building.getManager().getId());
+    }
+
+    public void addMessage(MessageAddDTO messageAddDTO, Long buildingId) {
+        BuildingEntity building = buildingService.findEntityById(buildingId);
+        UserEntity author = building.getManager();
+        MessageEntity newMessage = new MessageEntity()
+                .setAuthor(author)
+                .setBuilding(building)
+                .setTitle(messageAddDTO.getTitle())
+                .setContent(messageAddDTO.getContent())
+                .setCreatedDate(LocalDateTime.now());
+
+        buildingService.addMessage(newMessage, building);
+        userService.addMessageToUser(newMessage, author);
+
+        messageRepository.save(newMessage);
+    }
+
+    public MessageEntity findById(Long messageId) {
+        return messageRepository
+                .findById(messageId)
+                .orElseThrow(() -> new NullPointerException("No message with this Id"));
+    }
+
+    public void updateMessage(MessageEditDTO messageEditDTO) {
+        MessageEntity messageToUpdate = findById(messageEditDTO.getId());
+        messageToUpdate
+                .setTitle(messageEditDTO.getTitle())
+                .setContent(messageEditDTO.getContent());
+        messageRepository.save(messageToUpdate);
+    }
+
+    public void deleteMessageWithId(Long messageId, Long buildingId) {
+        MessageEntity messageToRemove = findById(messageId);
+        UserEntity author = messageToRemove.getAuthor();
+        BuildingEntity building = messageToRemove.getBuilding();
+
+        buildingService.removeMessageFromBuilding(messageToRemove, building);
+        userService.removeMessageFromUser(messageToRemove, author);
+
+        messageRepository.delete(messageToRemove);
     }
 }
