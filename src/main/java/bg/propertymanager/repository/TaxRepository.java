50fef8671ca11004d49a95730173fc9cdba38968
dimov05 <ddimov.1999@gmail.com>
2023-01-11 13:35:16 +0200

    Working on tax view and functionalities

--- src/main/java/bg/propertymanager/repository/TaxRepository.java
+++ src/main/java/bg/propertymanager/repository/TaxRepository.java
@@ -5,6 +5,9 @@ import bg.propertymanager.model.entity.TaxEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 @Repository
 public interface TaxRepository extends JpaRepository<TaxEntity, Long> {
+    Set<TaxEntity> findAllByBuilding_Id(Long buildingId);
 }
--- src/main/java/bg/propertymanager/service/TaxService.java
+++ src/main/java/bg/propertymanager/service/TaxService.java
@@ -1,7 +1,35 @@
 package bg.propertymanager.service;
 
+import bg.propertymanager.model.dto.building.BuildingViewDTO;
+import bg.propertymanager.model.dto.expense.TaxAddDTO;
+import bg.propertymanager.model.dto.expense.TaxViewDTO;
+import bg.propertymanager.repository.TaxRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class TaxService {
+    private final TaxRepository taxRepository;
+    private final ModelMapper modelMapper;
+
+    public TaxService(TaxRepository taxRepository, ModelMapper modelMapper) {
+        this.taxRepository = taxRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    public List<TaxViewDTO> findAllTaxes(BuildingViewDTO building) {
+        return taxRepository
+                .findAllByBuilding_Id(building.getId())
+                .stream()
+                .map(tax -> modelMapper.map(tax, TaxViewDTO.class))
+                .collect(Collectors.toList());
+
+    }
+
+    public void addTax(TaxAddDTO taxAddDTO, Long buildingId) {
+    }
 }
