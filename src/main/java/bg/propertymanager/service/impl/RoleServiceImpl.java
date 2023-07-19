978a864d08c80a57a8c3eb1c6067aba593fe687d
Dimo Dimov <ddimov.1999@gmail.com>
2023-07-19 09:10:37 +0300

    Fix dependencies in build.gradle

--- src/main/java/bg/propertymanager/service/impl/RoleServiceImpl.java
+++ src/main/java/bg/propertymanager/service/impl/RoleServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 @Transactional
@@ -21,5 +22,10 @@ public class RoleServiceImpl implements RoleService {
     @Override
     public RoleEntity findRoleByName(String roleName) {
         return roleRepository.findByName(roleName);
+    }
+
+    @Override
+    public List<RoleEntity> findAll() {
+        return roleRepository.findAll();
     }
 }
